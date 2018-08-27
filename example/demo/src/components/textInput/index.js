05f433755e2b5f846f68c6e2817fa7f07318c8c9
jerrychir <jerrychir@jerrychirs-MacBook-Pro.local>
2018-08-27 09:13:46 +0800

    prepare textinput

--- example/demo/src/components/textInput/index.js
+++ example/demo/src/components/textInput/index.js
@@ -2,12 +2,14 @@
  * @Author: jerrychir 
  * @Date: 2018-08-25 22:06:01 
  * @Last Modified by: jerrychir
- * @Last Modified time: 2018-08-25 22:06:31
+ * @Last Modified time: 2018-08-27 09:13:40
  */
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styles from "./styles";
-import View from "../view/index";
+import View from "../view/index"; 
+import {TextInput as RNTextInput} from 'react-native'
+
 
 
 /**
