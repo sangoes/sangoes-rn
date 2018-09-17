db798df89b2d9877b8e60ea091a5dbbea5de8d64
jerrychir <jerrychir@163.com>
2018-09-17 09:25:14 +0800

    add model

--- example/demo/src/components/popover/index.js
+++ example/demo/src/components/popover/index.js
@@ -1,8 +1,7 @@
 /*
  * @Author: 驷爺.J.C 
- * @Date: 2018-09-17 08:59:54 
- * @Last Modified by: 驷爺.J.C
- * @Last Modified time: 2018-09-17 09:00:21
- * 
- * 富文本
+ * @Date: 2018-09-17 09:19:08 
+ * @Last Modified by:   驷爺.J.C 
+ * @Last Modified time: 2018-09-17 09:19:08 
+ * Popover
  */
---
+++ example/demo/src/components/tabs/styles.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 驷爺.J.C 
+ * @Date: 2018-09-17 09:20:25 
+ * @Last Modified by:   驷爺.J.C 
+ * @Last Modified time: 2018-09-17 09:20:25 
+ */
+import { StyleSheet } from "react-native";
+
+const hairlineWidth = StyleSheet.hairlineWidth;
+
+const styles = StyleSheet.create({});
+
+export default styles;
